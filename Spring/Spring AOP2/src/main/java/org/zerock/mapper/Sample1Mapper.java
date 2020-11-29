2bcce4e96eda9cdfcd1a20f42dc96ae9c87717f5
jeon-yongho <yhjun1000@naver.com>
2020-11-29 19:26:04 +0900

    project modify

--- Spring/Spring AOP2/src/main/java/org/zerock/mapper/Sample1Mapper.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.mapper;
-
-import org.apache.ibatis.annotations.Insert;
-
-public interface Sample1Mapper {
-	@Insert("insert into tbl_sample1 (col1) values (#{data}) ")
-	public int inserCol1(String data);
-
-}
--- Spring/Spring AOP2/src/main/java/org/zerock/service/SampleTxSerice.java
+++
@@ -1,5 +0,0 @@
-package org.zerock.service;
-
-public interface SampleTxSerice {
-	public void addData(String value);
-}
