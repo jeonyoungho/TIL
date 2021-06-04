f11e371eb4f96ab548e5c3d7bdd817ee8e49b565
jeon-yongho <yhjun1000@gmail.com>
2021-06-04 17:53:08 +0900

    update N+1ProblemWithSpringBoot

--- Spring/JPA/N+1문제/N+1ProblemTestWithSpringBoot/src/main/java/com/example/OrmTestWithSpringApplication.java
+++ Spring/JPA/N+1문제/N+1ProblemTestWithSpringBoot/src/main/java/com/example/OrmTestWithSpringApplication.java
@@ -27,58 +27,58 @@ public class OrmTestWithSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        // Initialize Category and Product
-//        Category category1 = Category.builder()
-//                .name("Computer")
-//                .build();
-//        Product product1 = Product.builder()
-//                .name("Desktop")
-//                .price(150)
-//                .description("Powerful PC")
-//                .category(category1)
-//                .build();
-//        Product product2 = Product.builder()
-//                .name("Notebook")
-//                .price(200)
-//                .description("Awesome notebook")
-//                .category(category1)
-//                .build();
-//
-//        category1.getProducts().add(product1);
-//        category1.getProducts().add(product2);
-//
-//        Category category2 = Category.builder()
-//                .name("Car")
-//                .build();
-//
-//        Product product3 = Product.builder()
-//                .name("Sonata")
-//                .price(2000)
-//                .description("Popular car")
-//                .category(category2)
-//                .build();
-//
-//        category2.getProducts().add(product3);
-//
-//        categoryRepository.save(category1);
-//        categoryRepository.save(category2);
-//
-////        productRepository.save(product1);
-////        productRepository.save(product2);
-////        productRepository.save(product3);
-//
-//        // Initialize Academy and Subject
-//        List<Academy> academies = new ArrayList<>();
-//
-//        for(int i=0;i<10;i++) {
-//            Academy academy = Academy.builder()
-//                    .name("강남스쿨" + i)
-//                    .build();
-//
-//            academy.addSubject(Subject.builder().name("자바웹개발" + i).build());
-//            academies.add(academy);
-//        }
-//
-//        academyRepository.saveAll(academies);
+        // Initialize Category and Product
+        Category category1 = Category.builder()
+                .name("Computer")
+                .build();
+        Product product1 = Product.builder()
+                .name("Desktop")
+                .price(150)
+                .description("Powerful PC")
+                .category(category1)
+                .build();
+        Product product2 = Product.builder()
+                .name("Notebook")
+                .price(200)
+                .description("Awesome notebook")
+                .category(category1)
+                .build();
+
+        category1.getProducts().add(product1);
+        category1.getProducts().add(product2);
+
+        Category category2 = Category.builder()
+                .name("Car")
+                .build();
+
+        Product product3 = Product.builder()
+                .name("Sonata")
+                .price(2000)
+                .description("Popular car")
+                .category(category2)
+                .build();
+
+        category2.getProducts().add(product3);
+
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+
+//        productRepository.save(product1);
+//        productRepository.save(product2);
+//        productRepository.save(product3);
+
+        // Initialize Academy and Subject
+        List<Academy> academies = new ArrayList<>();
+
+        for(int i=0;i<10;i++) {
+            Academy academy = Academy.builder()
+                    .name("강남스쿨" + i)
+                    .build();
+
+            academy.addSubject(Subject.builder().name("자바웹개발" + i).build());
+            academies.add(academy);
+        }
+
+        academyRepository.saveAll(academies);
     }
 }
